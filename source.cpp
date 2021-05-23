2732163af0a3dc06d93e0f2993610ab5f0097576
wzjiujiu <62519540+wzjiujiu@users.noreply.github.com>
2021-05-23 17:49:04 +0200

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -8,15 +8,21 @@
 #include <string>
 #define N 50
 #define BUF 200
+#include "prof.h"
+#include "aula.h"
 
 
 using namespace std;
 list <Studente> getstudenti(char* filename);
+list<Prof>getprofs(char* filename);
+list<Aula>getaule(char* filename);
 
 int main(int argc, char** argv)
 {
 	list<Studente> listastudenti;
 	char *filename;
+	list<Prof> listprofessori;
+	list<Aula> listaule;
 	
 	/*if (argc <= 2)
 	{
@@ -29,6 +35,13 @@ int main(int argc, char** argv)
 		//{
 	filename = _strdup("db_studenti.txt");
     listastudenti = getstudenti(filename);
+	free(filename);
+	filename = _strdup("db_professori.txt");
+	listprofessori = getprofs(filename);
+	free(filename);
+	filename = _strdup("db_aule.txt");
+	listaule = getaule(filename);
+	
 		//}
 	//}
 
@@ -37,7 +50,7 @@ int main(int argc, char** argv)
 
 list <Studente> getstudenti(char* filename)
 {
-	int count = 0,i,matricolanum;
+	int i,matricolanum;
 	list<Studente> listastudenti;
 	vector<string> vect;
 	char* res;
@@ -78,4 +91,85 @@ list <Studente> getstudenti(char* filename)
 	}
 
 	return listastudenti;
+}
+
+list<Prof>getprofs(char* filename)
+{
+	int i, matricolanum;
+	list<Prof> listaprofessori;
+	vector<string> vect;
+	char* res;
+	char buf[BUF];
+	char* matricola;
+	Prof p;
+	FILE* fp;
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+	{
+		printf("apertura file fallito\n");
+		exit(0);
+	}
+	while (1)
+	{
+		res = fgets(buf, BUF, fp);
+		if (res == NULL)
+			break;
+		string data = res;
+		std::stringstream ss(data);
+		while (ss.good())
+		{
+			string substr;
+			getline(ss, substr, ';');
+			vect.push_back(substr);
+		}
+		matricola = _strdup(vect[0].c_str());
+		matricolanum = atoi(matricola + 1);
+		if (matricolanum % 2 != 0)
+		{
+			free(matricola);
+			printf("trovato matricola con numero dispari, nome prof : %s, per favore ricontrolla il file", vect[1].c_str());
+			exit(0);
+		}
+		p.define(vect[0], vect[1], vect[2], vect[3]);
+		listaprofessori.push_back(p);
+		vect.clear();
+	}
+
+	return listaprofessori;
+}
+
+
+list<Aula>getaule(char* filename)
+{
+	list<Aula> listaule;
+	Aula a;
+	vector<string> vect;
+	char* res;
+	char buf[BUF];
+	FILE* fp;
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+	{
+		printf("apertura file fallito\n");
+		exit(0);
+	}
+	while (1)
+	{
+		res = fgets(buf, BUF, fp);
+		if (res == NULL)
+			break;
+		string data = res;
+		std::stringstream ss(data);
+		while (ss.good())
+		{
+			string substr;
+			getline(ss, substr, ';');
+			vect.push_back(substr);
+		}
+		
+		a.define(vect[0], vect[1], vect[2], atoi(vect[3].c_str()),atoi(vect[4].c_str()));
+		listaule.push_back(a);
+		vect.clear();
+	}
+	return listaule;
 }
