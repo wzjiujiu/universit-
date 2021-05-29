77ac16ae47212b0f2996511351eca6a502084102
wzjiujiu <62519540+wzjiujiu@users.noreply.github.com>
2021-05-29 18:06:04 +0200

    Update prof.cpp

--- prof.cpp
+++ prof.cpp
@@ -34,3 +34,55 @@ std::string Prof::getemail()
 {
 	return this->eamil;
 }
+
+std::string Prof::generatematricola()
+{
+	int num = rand() % 100000;
+	while (num % 2 != 0)
+	{
+		num = rand() % 100000;
+	}
+	if (num < 100000)
+	{
+		if (num < 10)
+		{
+			num = num * 100000;
+		}
+		else if (num < 100)
+		{
+			num = num * 10000;
+		}
+		else if (num < 1000)
+		{
+			num = num * 1000;
+
+		}
+		else if (num < 10000)
+		{
+			num = num * 100;
+		}
+		else if (num < 100000)
+		{
+			num = num * 10;
+		}
+
+	}
+	std::stringstream ss;
+	ss << num;
+	std::string partenum = ss.str();
+	std::string matricola = "d" + partenum;
+	return matricola;
+}
+
+void Prof::updatename(std::string profnome)
+{
+	this->profname = profname;
+}
+void Prof::updatesurname(std::string profsurname)
+{
+	this->profsurname = profsurname;
+}
+void Prof::updateemail(std::string email)
+{
+	this->eamil = email;
+}
